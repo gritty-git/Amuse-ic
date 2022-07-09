bec69974b185f4ef9858fa6c638e03392520259e
gritty-git <62741960+gritty-git@users.noreply.github.com>
2022-07-09 12:41:27 +0530

    removed limit on number of songs on list

--- backend/graph.js
+++ backend/graph.js
@@ -18,13 +18,13 @@ module.exports = {
     const client = getAuthenticatedClient(msalClient, userId);
 
     const data = await client
-      .api('/me/drive/root:/Music:/children?$expand=thumbnails')
+      .api('/me/drive/root:/Music:/children?$expand=thumbnails&$top=500')
       .get();
     const newdata = [];
     //data.value.map(dt => newdata.push({"tn":(dt.thumbnails.length==0)?[]:dt.thumbnails[0].large.url}))
     data.value.map(dt => newdata.push({"id": dt.id, "name":dt.description, "metadata" :dt.audio,"webContentLink" : dt['@microsoft.graph.downloadUrl'], "thumbnail":(dt.thumbnails.length==0)?[]:dt.thumbnails[0].large.url }));
     //console.log(newdata);
-    return newdata;
+    return data;
   },
   
 };

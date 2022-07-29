4941815be3e1ca19b8deb18590d09cc436cd64b1
gritty-git <62741960+gritty-git@users.noreply.github.com>
2022-07-29 18:46:24 +0530

    changed user interface slightly

--- amuse-ic/src/AudioPlayer.js
+++ amuse-ic/src/AudioPlayer.js
@@ -32,31 +32,34 @@ function AudioPlayer() {
   },[]);
   if(isLoading){
     console.log("loading");
-    return <div className="login-page">
+    return <div className="">
     <Header/>
-    Loading...
-    <br></br>
-    (If not logged in then Login here!)
-    <br></br>
-    <a href="/auth/signin" className="btn btn-primary btn-block btn-lg">Login</a>
-    <br></br>
-    <h5>To do before log-in</h5>
-    <li>Create a folder and rename it to "Music" inside your root folder of OneDrive.</li>
-    <li>Put the mp3 files inside this folder to which you want to listen to.</li>
-    <br></br>
-    (If logged in then please wait for atleast 5 sec for the song to load!)
-    <br></br>
-    <br></br>
-    If you don't want to sign in and rather listen to my selected songs, use the following Microsoft credentials : 
-    <br></br>
-    Email : soumyamusicdump@gmail.com<br/>
-    Password : musicdumpsoumya123$<br/>
-    {/* <button className="btn btn-primary" onClick={() => { 
-    getSongList(jsonData);
-    setLoading(false);
-    }}>
-      Click Here!
-    </button>  */}
+    <div className='login-page'>
+      Loading...
+      <br></br>
+      (If not logged in then Login here!)
+      <br></br>
+      <a href="/auth/signin" className="btn btn-primary btn-block btn-lg">Login</a>
+      <br></br>
+      <h5>To do before log-in</h5>
+      <li>Create a folder and rename it to "Music" inside your root folder of OneDrive.</li>
+      <li>Put the mp3 files inside this folder to which you want to listen to.</li>
+      <br></br>
+      (If logged in then please wait for atleast 5 sec for the song to load!)
+      <br></br>
+      <br></br>
+      If you don't want to sign in and rather listen to my selected songs, use the following Microsoft credentials : 
+      <br></br>
+      Email : soumyamusicdump@gmail.com<br/>
+      Password : musicdumpsoumya123$<br/>
+      {/* <button className="btn btn-primary" onClick={() => { 
+      getSongList(jsonData);
+      setLoading(false);
+      }}>
+        Click Here!
+      </button>  */}
+    </div>
+    
     </div>;
     
   }
--- amuse-ic/src/components/graphics/Header.jsx
+++ amuse-ic/src/components/graphics/Header.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 function Header() {  
     return (
-      <header className="draggable">
-        <h3>AMUSE-IC</h3>
+      <header className="draggable header">
+        <h3 style={{"float":"left"}}>AMUSE-IC</h3>
+        <a style={{"float":"right"}} href="/auth/signin" className="btn btn-primary btn-block">Login</a>
       </header>
     )
   }

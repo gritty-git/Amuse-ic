a9a1f48430444788b207dc27bebf6b598959f774
gritty-git <62741960+gritty-git@users.noreply.github.com>
2022-07-31 10:27:31 +0530

    added logout3

--- backend/app.js
+++ backend/app.js
@@ -118,7 +118,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  //res.render('error');
 });
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, '../amuse-ic/build')));

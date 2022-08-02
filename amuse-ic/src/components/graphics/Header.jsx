4a4b78f29ae52e00af1c2cf6ff1e03e743d96a92
gritty-git <62741960+gritty-git@users.noreply.github.com>
2022-08-02 22:41:21 +0530

    added dropdown logout

--- amuse-ic/src/components/graphics/Header.jsx
+++ amuse-ic/src/components/graphics/Header.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React,{useState} from "react";
 
 function Header(props) {  
+  const [isDropdownOpened , setDropdownOpen ] = useState(false);
+  const toggleDropDown = () => setDropdownOpen(!isDropdownOpened);
     return (
       <header className="draggable header">
-        <h3 style={{"float":"left"}}>AMUSE-IC</h3>
+        <h3 style={{"float":"left","fontWeight":"bold"}}>AMUSE-IC</h3>
         {props.loggedIn
           ?
-          <a style={{"float":"right"}} href="/auth/signout" className="">{props.username}</a>
+          <div style={{"float":"right"}}>
+          
+          <button type="button" onClick={toggleDropDown} class="btn btn-success">{props.username}</button>
+          {isDropdownOpened ? <div style={{"backgroundColor":"whitesmoke"}}><a href="/auth/signout">Sign Out</a></div>:<></>}
+          </div>
           :
           <a style={{"float":"right"}} href="/auth/signin" className="btn btn-primary btn-block">Login</a>
         }

30f10e165911ed12fefc92cc8edd5f43703709f1
gritty-git <62741960+gritty-git@users.noreply.github.com>
2023-01-19 18:10:07 +0530

    deployment changes

--- amuse-ic/src/AudioPlayer.js
+++ amuse-ic/src/AudioPlayer.js
@@ -1,47 +1,45 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-import { song_list } from './context/songs'
+import { song_list } from "./context/songs";
 import axios from "axios";
-import './main.css'
-import './input.css'
-import 'bootstrap/dist/css/bootstrap.css';
-import Login from './components/views/Login';
-import PlayerInterface from './components/views/PlayerInterface';
-
-
+import "./main.css";
+import "./input.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Login from "./components/views/Login";
+import PlayerInterface from "./components/views/PlayerInterface";
 
 function AudioPlayer() {
-  var [songList,getSongList] = useState(song_list);
+  var [songList, getSongList] = useState(song_list);
   const [loggedIn, setLoggedIn] = useState(false);
   const [fetching, setFetching] = useState(false);
   const [username, setUsername] = useState("User");
   useEffect(() => {
     setFetching(true);
-    
-    axios.get('/filedata')
-    .then(res => {
-      
-      setFetching(false);
-      if(Array.isArray(res.data)){
-        setUsername(prevValue => {return res.data[res.data.length-1]});
-        
-        getSongList(res.data.slice(0,-1));
-        setLoggedIn(true);
-      }else{
-        
-      }
-    }).catch(err => {
-      console.log(err);
-      setFetching(false);
-    });
-  },[]);
-  if(!loggedIn){
-    return (<Login fetching={fetching} username={username}/>);
+
+    axios
+      .get("/filedata")
+      .then((res) => {
+        setFetching(false);
+        if (Array.isArray(res.data)) {
+          setUsername((prevValue) => {
+            return res.data[res.data.length - 1];
+          });
+
+          getSongList(res.data.slice(0, -1));
+          setLoggedIn(true);
+        } else {
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        setFetching(false);
+      });
+  }, []);
+  if (!loggedIn) {
+    return <Login fetching={fetching} username={username} />;
   }
 
-  return (
-    <PlayerInterface songList={songList} username={username}/>
-  );
+  return <PlayerInterface songList={songList} username={username} />;
 }
 
 export default AudioPlayer;
--- backend/routes/filedata.js
+++ backend/routes/filedata.js
@@ -1,33 +1,44 @@
-const router = require('express-promise-router')();
-const graph = require('../graph.js');
+const router = require("express-promise-router")();
+const graph = require("../graph.js");
 
-router.get('/',
-  async function(req, res, next) {
-    if (!req.session.userId) {
-      // Redirect unauthenticated requests to home page
-      res.redirect('/index')
-    } else {
-      const params = {
-        active: { drive: true }
-      };
+router.get("/", async function (req, res, next) {
+  //console.log(req.app.locals);
+  if (
+    !req.session.userId ||
+    !req.app.locals.users ||
+    Object.keys(req.app.locals.users).length === 0
+  ) {
+    //console.log("redirected to index");
+    // Redirect unauthenticated requests to home page
+    // res.redirect("auth/signin");
+    res.status(401);
+    res.send("401");
+    //console.log("unauthorise");
+    //throw new Error("Not authorized.");
+  } else {
+    const params = {
+      active: { drive: true },
+    };
 
-      // Get the user
-      const user = req.app.locals.users[req.session.userId];
-      try {
-        // Get the events
-        
-        const details = await graph.getFileDetails(
-          req.app.locals.msalClient,
-          req.session.userId,
-          );
-          details.push(req.app.locals.users[req.session.userId].displayName.length===0?req.app.locals.users[req.session.userId].email:req.app.locals.users[req.session.userId].displayName);
-        res.json(details);
-      } catch (err) {
-        res.send(JSON.stringify(err, Object.getOwnPropertyNames(err)));
-      }
+    // Get the user
+    const user = req.app.locals.users[req.session.userId];
+    try {
+      // Get the events
+
+      const details = await graph.getFileDetails(
+        req.app.locals.msalClient,
+        req.session.userId
+      );
+      details.push(
+        req.app.locals.users[req.session.userId].displayName.length === 0
+          ? req.app.locals.users[req.session.userId].email
+          : req.app.locals.users[req.session.userId].displayName
+      );
+      res.json(details);
+    } catch (err) {
+      res.send(JSON.stringify(err, Object.getOwnPropertyNames(err)));
     }
   }
-);
-
+});
 
 module.exports = router;
